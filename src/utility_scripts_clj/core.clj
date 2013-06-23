4b14bb7cdc5aeb0a9fae7db1daff330f9418ce4e
EFulmer <EFulmer@gmail.com>
2013-06-23 14:53:11 -0400

    first commit

---
+++ src/utility_scripts_clj/core.clj
@@ -0,0 +1,16 @@
+(ns utility-scripts-clj.core
+  (:gen-class)
+  (:import java.io.File))
+
+(defn delete-temp-files 
+  "Deletes all the temporary files in a given path and its subdirectories.
+path should be a valid java.io.File object."
+  [path]
+  (let [dir (file-seq path)
+        is-temp-file (fn [file-name] (and (.isFile file-name) (.endsWith (.getName file-name) "~")))]
+    (map (memfn delete) (filter is-temp-file dir))))
+
+(defn -main
+  "Takes a pathname and calls delete-temp-files on it."
+  [path]
+  (delete-temp-files (File. path)))
